dd20d900b56fcc851e432f5feab85b9b721f3951
Juan Blanco <juanblancodev@gmail.com>
2024-04-11 16:57:00 -0700

    h2 semantics updated to h1 and font-size styled added

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
     <div className="grid p-items-center gap-1">
-      <h2 className="color-white">Jessica Randall</h2>
+      <h1 className="font-m color-white">Jessica Randall</h1>
       <p className="color-primary bold">London, United Kingdom</p>
     </div>
   )
