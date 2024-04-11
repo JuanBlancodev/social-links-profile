f8e28f610c8a34fe5223f27c0e34d7a3d94232cd
Juan Blanco <juanblancodev@gmail.com>
2024-04-11 02:17:53 -0700

    container added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,31 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+`
+
+const SubContainer = styled.div`
+  margin-top: 70px;
+  width: 400px;
+  height: 600px;
+
+  border-radius: 10px;
+  background-color: var(--color-dark-rey);
+
+  @media (height < 740px){
+    margin-top: 10px;
+  }
+`
+
 const App = () => {
   return (
-    <div>App</div>
+    <Container>
+      <SubContainer />
+    </Container>
   )
 }
 
