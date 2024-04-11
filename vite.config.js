a2f05296ca1250e483dc24f847959412303f62c8
Juan Blanco <juanblancodev@gmail.com>
2024-04-11 03:50:46 -0700

    postcss & terser added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    terser()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ]
+    }
+  },
+  base: 'https://juanblancodev.github.io/social-links-profile/'
 })
